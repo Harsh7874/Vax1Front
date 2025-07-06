5ecc07d0ae40f519fc8ae5f90f0f28b9097f81be
Harsh7874 <142920362+Harsh7874@users.noreply.github.com>
2025-07-07 00:49:21 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = 'http://localhost:4000'
+    const backendUrl = 'https://vax01back.onrender.com'
 
     // const [doctors, setDoctors] = useState([])
     const [hospitals, setHospitals] = useState([])
